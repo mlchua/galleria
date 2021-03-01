23e9ee00d8ad6e51039d77a11b8eb2cf5b7575f3
Mark Chua <marklimuelchua@gmail.com>
2021-02-28 19:59:48 -0500

    Initial antd component setup

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import { Layout, Menu } from 'antd';
 import logo from './logo.svg';
 import './App.css';
+import { Footer } from 'antd/lib/layout/layout';
+
+const { Header, Content, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0
+        }}
+      >
+        <Menu theme="dark" mode="inline">
+          <Menu.Item key="Test-menu-item">
+            Test menu item
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div className="site-layout-background" style-={{ padding: 24, textAlign: 'center' }}>
+            Test
+          </div>
+        </Content>
+        <Footer></Footer>
+      </Layout>
+    </Layout>
   );
 }
 
